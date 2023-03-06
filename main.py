ffdd4f2def6975bc4f9f0e834e15d4e864ae1549
Abdulrahman Ademidun Adeyemi <105551933+deyem1@users.noreply.github.com>
2023-03-06 21:05:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ import streamlit as st
 #     engine.runAndWait()
 
 # talk('i am  here')
+# @st.cache
+# def download1(url1):
+#     url = url1
+#     filename = url.split('/')[-1]
+#     urllib.request.urlretrieve(url, filename)
 
 try:
 
@@ -28,19 +33,19 @@ try:
     import cv2
     import tensorflow as tf
 
-
     st.title('Braille Detector:book:')
     # st.title(' ')
     st.markdown("<h3 style='text-align: center; '></h3>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center; '>This program checks an image to determine if there is braille within an image</p>", unsafe_allow_html=True)
+    st.markdown(
+        "<p style='text-align: center; '>This program checks an image to determine if there is braille within an image</p>",
+        unsafe_allow_html=True)
     st.markdown("<h3 style='text-align: center; '></h3>", unsafe_allow_html=True)
 
     from gtts import gTTS
 
-
     from io import BytesIO
-    sound_file = BytesIO()
 
+    sound_file = BytesIO()
 
 
     def predict_upload():
@@ -49,14 +54,17 @@ try:
 
         yhat = new_model.predict(np.expand_dims(resize / 120, 0))
         print(yhat)
-        ans = round(yhat[0][0][0]*100, 2)
+        ans = round(yhat[0][0][0] * 100, 2)
         print('probability value:', yhat[0])
         if yhat[0] < 0.6:
-            text = f'The uploaded image does not have braille ❌.\n There is a {ans}% chance that this picture has braille '
+            text = f'The uploaded image does not have braille ❌.\n There is a {ans}% chance that this picture has ' \
+                   f'braille '
             # print(f'Predicted class does not have braille')
             st.error(text)
             print(text)
-            tts = gTTS(f'The uploaded image does not have braille. There is a {ans}% chance that this picture has braille.' , lang='en')
+            tts = gTTS(
+                f'The uploaded image does not have braille. There is a {ans}% chance that this picture has braille.',
+                lang='en')
             tts.write_to_fp(sound_file)
             st.audio(sound_file)
             # talk('image does not have braille')
@@ -72,7 +80,6 @@ try:
 
     # def uploadingFile():
     #
-
 
     typer = st.radio(
         "Select an option to upload media",
@@ -102,180 +109,7 @@ try:
         else:
             print('no image')
 
-    # genre = st.radio(
-
-    #     "What\'s your favorite movie genre",
-    #     ('Comedy', 'Drama', 'Documentary'))
-    #
-    # if genre == 'Comedy':
-    #     st.write('You selected comedy.')
-    #     # uploadingFile()
-    # else:
-    #     st.write("You didn\'t select comedy.")
 
 
 except:
     print('errorrrrr1')
-
-# import wikipedia
-# import csv
-# from datetime import date
-#
-# import datetime
-# import pywhatkit
-# import PyAudio
-# ass_Name = "alexa"
-# def take_command():
-#     try:
-#         with sr.Microphone() as source:
-#             print("This is Adeyemi's Voice Assistant")
-#             print("listening....")
-#             print("Please speak now!")
-#             voice = listener.listen(source)
-#             command = listener.recognize_google(voice)
-#             command = command.lower()
-#             # command = command.replace('snow', '')
-#             return command
-#
-#             # if "snow" in command:
-#             #     # talk("This is Adeyemi's Voice Assistant called snow")
-#             #     # talk("what would you like me to do")
-#             #
-#             #     talk('what would you like me to do')
-#             #     talk("hello")
-#             #     print(command)
-#             #     return command
-#             # else:
-#             #     talk('my name is snow')
-#
-#                 # return command
-#             # talk("This is Adeyemi's Voice Assistant called snow")
-#     except:
-#         pass
-#
-# #
-# def wake_up_call():
-#     command = take_command()
-#     command = command.lower()
-#     if "snow" in command:
-#         # talk("This is Adeyemi's Voice Assistant called snow")
-#         # talk("what would you like me to do")
-#         command = command.replace('snow', '')
-#
-#         talk("hello")
-#         talk('what would you like me to do')
-#
-#         print("wake up call activated")
-#         # other_command()
-#         run_snow()
-#         # return other_command()
-#         # take_command()
-#     else:
-#         talk('my name is snow')
-#         print(command)
-#
-#
-#
-# def onboard():
-#     talk('Who do you want to onboard?')
-#     new_employee = other_command()
-#     # new_employee = new_employee.lower()
-#     print(new_employee)
-#     talk("is the name of the new employee " + new_employee)
-#     talk('is that correct?')
-#     print('5')
-#     # new_employee = onboard()
-#     answer = other_command()
-#     answer = answer.lower()
-#     if answer == 'yes':
-#         talk('Creating contract for' + new_employee)
-#
-#         talk('what department?')
-#         new_department = other_command()
-#
-#         talk('which city branch?')
-#         new_branch = other_command()
-#
-#
-#
-#         today = date.today()
-#
-#
-#
-#     elif answer == 'no':
-#         # new_employee = other_command()
-#         # onboard()
-#         # talk('please repeat the name of who you would like to on board')
-#         # new_employee = other_command()
-#         # new_employee = new_employee.lower()
-#
-#         onboard()
-#         # repeat_onboard()
-#     else:
-#         talk('is that a yes or a no')
-#         onboard()
-#
-#     return new_employee
-#
-# # def repeat_onboard():
-#
-#
-#
-#
-# def run_snow():
-#     command = other_command()
-#     # command = other_command().lower()
-#     print(command)
-#     # if "play" in command:
-#     #     song = command.replace('play', "")
-#     #     pywhatkit.playonyt(song)
-#     #
-#     #     talk("playing" + song)
-#     #     # talk("playing " + song)
-#     #     print(song)
-#     if 'time' in command:
-#         time = datetime.datetime.now().strftime('%I: %M %p')
-#         talk("i do not know the time, just joking! fetching current time")
-#         talk("current time is " + time)
-#         print(time)
-#
-#     elif "what is" in command:
-#         # 'wikipedia' in command or "who is" in command or
-#         person = command.replace("what is", '')
-#         info = wikipedia.summary(person, 1)
-#         print(info)
-#         talk(info)
-#
-#
-#
-#
-#
-#
-#     else:
-#         talk("you said " + command + " but sorry, i can't do that right now")
-#         run_snow()
-#
-#
-#
-#
-# # with sr.Microphone() as source:
-# #     print("This is Adeyemi's Voice Assistant")
-# #     print("listening....")
-# #     print("Please speak now!")
-# #     voice = listener.listen(source)
-# #     command = listener.recognize_google(voice)
-# #     command = command.lower()
-# #     talk("This is Adeyemi's Voice Assistant called snow")
-# #     talk("i do not know the time, just joking! fetching current time")
-#
-#
-#
-#
-#
-#
-#
-# wake_up_call()
-# # run_snow()
-# # take_command()
-# contract()
-#
